1c1f5043449cb7f85e074202f26b234fe23d5818
Mario G. Pozzo <camboris@gmail.com>
2016-05-28 18:58:40 -0300

    Ajustes de scripts i3blocks
    algunos scripts no funcionaban bien por rutas mal puestas

--- i3blocks/scripts/keyboard.sh
+++ i3blocks/scripts/keyboard.sh
@@ -1,6 +1,23 @@
-#! /bin/sh
+#!/usr/bin/env bash
 
-LAYOUT=$(setxkbmap -query | awk '/layout/{print $2}')
-VARIANT=$(setxkbmap -query | awk '/variant/{print $2}')
+# Copyright 2016 Patrick Haun
+# Edited: Denis Kadyshev
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 
-echo "$LAYOUT:$VARIANT"
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+setxkbmap -query | awk '
+    BEGIN{layout="";variant=""}
+    /^layout/{layout=$2}
+    /^variant/{variant=$2}
+    END{printf("%s %s",layout,variant)}'
