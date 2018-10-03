f5370026ae8daed30f17f0f6797b6a6a148e54bf
Mario G. Pozzo <camboris@gmail.com>
2018-10-03 15:10:35 -0300

    agrego toggle monitor ofi

---
+++ bin/toggle-ofi.sh
@@ -0,0 +1,7 @@
+if xrandr | grep "eDP-1 connected"
+then
+    xrandr --output DP-1 --mode 1920x1080 --pos 1366x0 --rotate normal --output eDP-1 --primary --mode 1366x768 --pos 0x312 --rotate normal
+else
+    xrandr --output eDP-1 --auto --output DP-1 --off
+fi
+feh --bg-max ~/dotfiles/i3extra/fondo-ballena.jpg
