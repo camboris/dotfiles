61c178aaa527af574b143c4ed74bb52fec659cea
Mario G. Pozzo <camboris@gmail.com>
2018-10-31 15:41:18 -0300

    Fix script monitor ofi

--- bin/toggle-ofi.sh
+++ bin/toggle-ofi.sh
@@ -1,4 +1,4 @@
-if xrandr | grep "eDP-1 connected"
+if xrandr | grep "^DP-1 connected"
 then
     xrandr --output DP-1 --mode 1920x1080 --pos 1366x0 --rotate normal --output eDP-1 --primary --mode 1366x768 --pos 0x312 --rotate normal
 else
