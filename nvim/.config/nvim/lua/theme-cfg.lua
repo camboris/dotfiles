1b5f64e8ac95da9d19787c635cde7d84e713b925
Mario G. Pozzo <mario.pozzo@mercadolibre.com>
2021-08-03 12:01:43 -0300

    separar

---
+++ nvim/.config/nvim/lua/theme-cfg.lua
@@ -0,0 +1,6 @@
+local g = vim.g
+local cmd = vim.cmd
+
+-- Available values: `'default'`, `'atlantis'`, `'andromeda'`, `'shusia'`, `'maia'`, `'espresso'`
+g.sonokai_style = 'espresso'
+cmd 'colorscheme sonokai'
