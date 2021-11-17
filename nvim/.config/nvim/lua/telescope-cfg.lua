25cb96074a545c3c3f019dc7b0da51b5891677b0
Mario G. Pozzo <mario.pozzo@mercadolibre.com>
2021-11-17 14:51:42 -0300

    cambia estrategia layout telescope

--- nvim/.config/nvim/lua/telescope-cfg.lua
+++ nvim/.config/nvim/lua/telescope-cfg.lua
@@ -1,6 +1,6 @@
 --Add leader shortcuts
 vim.api.nvim_set_keymap('n', '<leader><space>', [[<cmd>lua require('telescope.builtin').buffers()<CR>]], { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>sf', [[<cmd>lua require('telescope.builtin').find_files({previewer = false})<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>sf', [[<cmd>lua require('telescope.builtin').find_files()<CR>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>sb', [[<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<CR>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>sh', [[<cmd>lua require('telescope.builtin').help_tags()<CR>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>st', [[<cmd>lua require('telescope.builtin').tags()<CR>]], { noremap = true, silent = true })
@@ -9,4 +9,47 @@ vim.api.nvim_set_keymap('n', '<leader>sp', [[<cmd>lua require('telescope.builtin
 vim.api.nvim_set_keymap('n', '<leader>so', [[<cmd>lua require('telescope.builtin').tags{ only_current_buffer = true }<CR>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>?', [[<cmd>lua require('telescope.builtin').oldfiles()<CR>]], { noremap = true, silent = true })
 
--- require('telescope').setup {}
+-- Telescope Globalremapping
+local action_state = require("telescope.actions.state")
+local actions = require("telescope.actions")
+require('telescope').setup {
+  defaults = {
+    layout_strategy = 'flex'
+  },
+  pickers = {
+    find_files = {
+      on_input_filter_cb = function(prompt)
+        local find_colon = string.find(prompt, ":")
+        if find_colon then
+          local ret = string.sub(prompt, 1, find_colon - 1)
+          vim.schedule(function()
+            local prompt_bufnr = vim.api.nvim_get_current_buf()
+            local picker = action_state.get_current_picker(prompt_bufnr)
+            local lnum = tonumber(prompt:sub(find_colon + 1))
+            if type(lnum) == "number" then
+              local win = picker.previewer.state.winid
+              local bufnr = picker.previewer.state.bufnr
+              local line_count = vim.api.nvim_buf_line_count(bufnr)
+              vim.api.nvim_win_set_cursor(win, { math.max(1, math.min(lnum, line_count)), 0 })
+            end
+          end)
+          return { prompt = ret }
+        end
+      end,
+      attach_mappings = function()
+        actions.select_default:enhance {
+          post = function()
+            -- if we found something, go to line
+            local prompt = action_state.get_current_line()
+            local find_colon = string.find(prompt, ":")
+            if find_colon then
+              local lnum = tonumber(prompt:sub(find_colon + 1))
+              vim.api.nvim_win_set_cursor(0, { lnum, 0 })
+            end
+          end,
+        }
+        return true
+      end,
+    },
+  }
+}
