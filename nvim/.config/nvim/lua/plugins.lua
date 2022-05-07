140afce3b4a94c93e83ad7d4fbb363d7a37f8921
Mario G. Pozzo <camboris@gmail.com>
2022-05-07 15:57:07 -0300

    add more cfg files

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -44,7 +44,6 @@ packer.startup(function(use)
 
   use({ "kyazdani42/nvim-tree.lua", config = get_config("nvim-tree") })
 
-
   use({
     "nvim-lualine/lualine.nvim",
     config = get_config("lualine"),
@@ -62,12 +61,12 @@ packer.startup(function(use)
   use("nvim-treesitter/nvim-treesitter-textobjects")
   use("p00f/nvim-ts-rainbow")
 
-  use {
-    'numToStr/Comment.nvim',
+  use({
+    "numToStr/Comment.nvim",
     config = function()
-      require('Comment').setup()
-    end
-  }
+      require("Comment").setup()
+    end,
+  })
 
   use({ "neovim/nvim-lspconfig", config = get_config("lsp") })
 
@@ -75,7 +74,7 @@ packer.startup(function(use)
     "ray-x/lsp_signature.nvim",
     require = { "neovim/nvim-lspconfig" },
     config = get_config("lsp-signature"),
- })
+  })
 
   use({ "onsails/lspkind-nvim", requires = { "famiu/bufdelete.nvim" } })
 
@@ -85,11 +84,7 @@ packer.startup(function(use)
     config = get_config("null-ls"),
   })
 
-  use({
-    "akinsho/nvim-toggleterm.lua",
-    keys = { "<C-n>", "<leader>fl", "<leader>gt" },
-    config = get_config("toggleterm"),
-  })
+  use({ "akinsho/toggleterm.nvim", tag = "v1.*", config = get_config("toggleterm") })
 
   use({ "folke/which-key.nvim", config = get_config("which") })
 
@@ -102,34 +97,34 @@ packer.startup(function(use)
   use({ "rcarriga/nvim-notify", config = get_config("notify") })
 
   use({
-		"https://gitlab.com/yorickpeterse/nvim-window.git",
-		config = get_config("nvim-window"),
-	})
+    "https://gitlab.com/yorickpeterse/nvim-window.git",
+    config = get_config("nvim-window"),
+  })
 
   use({
-		"norcalli/nvim-colorizer.lua",
-		event = "BufReadPre",
-		config = get_config("colorizer"),
-	})
+    "norcalli/nvim-colorizer.lua",
+    event = "BufReadPre",
+    config = get_config("colorizer"),
+  })
 
-  	use({
-		"hrsh7th/nvim-cmp",
-		requires = {
-			{ "hrsh7th/cmp-nvim-lsp" },
-			{ "hrsh7th/cmp-buffer" },
-			{ "hrsh7th/cmp-path" },
-			{ "hrsh7th/cmp-cmdline" },
-      { 'hrsh7th/cmp-nvim-lua' };
-		},
-		config = get_config("cmp"),
-	})
+  use({
+    "hrsh7th/nvim-cmp",
+    requires = {
+      { "hrsh7th/cmp-nvim-lsp" },
+      { "hrsh7th/cmp-buffer" },
+      { "hrsh7th/cmp-path" },
+      { "hrsh7th/cmp-cmdline" },
+      { "hrsh7th/cmp-nvim-lua" },
+    },
+    config = get_config("cmp"),
+  })
 
-	use({ "rafamadriz/friendly-snippets" })
-	use({
-		"L3MON4D3/LuaSnip",
-		requires = "saadparwaiz1/cmp_luasnip",
-		config = get_config("luasnip"),
-	})
+  use({ "rafamadriz/friendly-snippets" })
+  use({
+    "L3MON4D3/LuaSnip",
+    requires = "saadparwaiz1/cmp_luasnip",
+    config = get_config("luasnip"),
+  })
 
   use({
     "lewis6991/gitsigns.nvim",
@@ -138,16 +133,15 @@ packer.startup(function(use)
     config = get_config("gitsigns"),
   })
 
-  use {
-    'goolord/alpha-nvim',
-    requires = { 'kyazdani42/nvim-web-devicons' },
+  use({
+    "goolord/alpha-nvim",
+    requires = { "kyazdani42/nvim-web-devicons" },
     config = get_config("alpha"),
-  }
+  })
 
-  use {
-    'phaazon/hop.nvim',
-    branch = 'v1', -- optional but strongly recommended
+  use({
+    "phaazon/hop.nvim",
+    branch = "v1", -- optional but strongly recommended
     config = get_config("hop"),
-  }
-
+  })
 end)
