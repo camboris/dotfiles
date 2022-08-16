23dc5314e7541005702ccde853d7aa8450f240cd
Mario G. Pozzo <camboris@gmail.com>
2022-08-16 13:49:51 -0300

    minor lsp formatting adjustments

--- nvim/.config/nvim/lua/config/null-ls.lua
+++ nvim/.config/nvim/lua/config/null-ls.lua
@@ -1,21 +1,23 @@
 local nls = require("null-ls")
 nls.setup({
-	sources = {
-		nls.builtins.formatting.stylua,
-		nls.builtins.diagnostics.eslint,
-		nls.builtins.code_actions.eslint,
-		nls.builtins.completion.spell,
-		nls.builtins.formatting.prettier,
+  sources = {
+    nls.builtins.formatting.stylua,
+    nls.builtins.diagnostics.eslint,
+    nls.builtins.code_actions.eslint,
+    nls.builtins.completion.spell.with({
+      filetypes = { "html", "json", "text", "norg", "markdown" },
+    }),
+    nls.builtins.formatting.prettier,
     nls.builtins.formatting.black,
-	},
-	on_attach = function(client)
-		if client.server_capabilities.document_formatting then
-			-- auto format on save (not asynchronous)
-			local LspFormattingGrp = vim.api.nvim_create_augroup("LspFormattingGrp", { clear = true })
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				command = "lua vim.lsp.buf.format()",
-				group = LspFormattingGrp,
-			})
-		end
-	end,
+  },
+  on_attach = function(client)
+    if client.server_capabilities.document_formatting then
+      -- auto format on save (not asynchronous)
+      local LspFormattingGrp = vim.api.nvim_create_augroup("LspFormattingGrp", { clear = true })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        command = "lua vim.lsp.buf.format()",
+        group = LspFormattingGrp,
+      })
+    end
+  end,
 })
