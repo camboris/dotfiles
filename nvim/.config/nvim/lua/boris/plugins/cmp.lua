bdc7b3daf62b629dc6d4d09638876f2a327f7d58
Mario G. Pozzo <camboris@gmail.com>
2023-02-01 14:56:48 -0600

    Agrega cmp base

---
+++ nvim/.config/nvim/lua/boris/plugins/cmp.lua
@@ -0,0 +1,49 @@
+local M = {
+  "hrsh7th/nvim-cmp",
+  event = "InsertEnter",
+  dependencies = {
+    { "hrsh7th/cmp-nvim-lsp" },
+    { "hrsh7th/cmp-buffer" },
+    { "hrsh7th/cmp-path" },
+    { "hrsh7th/cmp-cmdline" },
+    { "hrsh7th/cmp-nvim-lua" },
+  },
+  config = function ()
+    local cmp = require("cmp")
+    cmp.setup({
+      experimental = { native_menu = false, ghost_text = true },
+      mapping = {
+        ['<C-p>'] = cmp.mapping.select_prev_item(),
+        ['<C-n>'] = cmp.mapping.select_next_item(),
+        ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-u>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.close(),
+        ["<CR>"] = cmp.mapping.confirm({
+          behavior = cmp.ConfirmBehavior.Replace,
+          select = false,
+        }),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function()
+          if cmp.visible() then
+            cmp.select_prev_item()
+          end
+        end, { "i", "s" }),
+      },
+      sources = {
+        --	{ name = "nvim_lsp" },
+        { name = "buffer", keyword_length = 3 },
+        --	{ name = "luasnip" },
+        { name = "path" },
+      },
+    })
+  end
+}
+
+return M
