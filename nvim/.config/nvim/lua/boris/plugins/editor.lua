4da5e0f5a41b43a7e003424419b4365c7cec906a
Mario G. Pozzo <camboris@gmail.com>
2023-03-01 12:48:20 -0600

    mueve config del colorscheme a editor

--- nvim/.config/nvim/lua/boris/plugins/editor.lua
+++ nvim/.config/nvim/lua/boris/plugins/editor.lua
@@ -1,58 +1,66 @@
+local colorscheme = {
+  "EdenEast/nightfox.nvim",
+  config = function()
+    vim.cmd([[colorscheme duskfox]])
+  end
+}
+
 local leap = {
-    "ggandor/leap.nvim",
-    event = "VeryLazy",
-    config = function()
-      local leap = require("leap")
-      leap.add_default_mappings(true)
-    end
+  "ggandor/leap.nvim",
+  event = "VeryLazy",
+  config = function()
+    local leap = require("leap")
+    leap.add_default_mappings(true)
+  end
 }
 
 local indent = {
-    "lukas-reineke/indent-blankline.nvim",
-    event = "BufReadPre",
+  "lukas-reineke/indent-blankline.nvim",
+  event = "BufReadPre",
 }
 
 local zenmode = {
-    "folke/zen-mode.nvim",
-    cmd = "ZenMode",
-    config = function()
-      require("zen-mode").setup()
-    end
+  "folke/zen-mode.nvim",
+  cmd = "ZenMode",
+  config = function()
+    require("zen-mode").setup()
+  end
 }
 
 local colorizer = {
-    'NvChad/nvim-colorizer.lua',
-    event = "BufReadPre",
-    config = function()
-      require 'colorizer'.setup()
-    end
+  'NvChad/nvim-colorizer.lua',
+  event = "BufReadPre",
+  config = function()
+    require 'colorizer'.setup()
+  end
 }
 
 local surround = {
-    "kylechui/nvim-surround",
-    event = "BufReadPre",
-    config = function()
-      require("nvim-surround").setup({
-          -- Configuration here, or leave empty to use defaults
-      })
-    end
+  "kylechui/nvim-surround",
+  event = "BufReadPre",
+  config = function()
+    require("nvim-surround").setup({
+      -- Configuration here, or leave empty to use defaults
+    })
+  end
 }
 
 local tint = {
-    "levouh/tint.nvim",
-    event = "BufReadPre",
-    config = function()
-      require("tint").setup({
-          -- Configuration here, or leave empty to use defaults
-      })
-    end,
+  "levouh/tint.nvim",
+  event = "BufReadPre",
+  config = function()
+    require("tint").setup({
+      -- Configuration here, or leave empty to use defaults
+    })
+  end,
 }
 
 return {
-    colorizer,
-    indent,
-    leap,
-    surround,
-    tint,
-    zenmode
+  colorizer,
+  colorscheme,
+  indent,
+  leap,
+  surround,
+  tint,
+  zenmode,
 }
