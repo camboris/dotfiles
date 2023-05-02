bb0596413d421c608ae277ca6cf2ac0b235e8206
Mario G. Pozzo <camboris@gmail.com>
2023-05-02 16:55:34 -0600

    nnvim: format go files on save

--- nvim/.config/nvim/lua/autocmd.lua
+++ nvim/.config/nvim/lua/autocmd.lua
@@ -79,3 +79,13 @@ api.nvim_create_autocmd("TermOpen", {
   end,
   desc = "Mappings for navigation with a terminal",
 })
+
+-- Run gofmt on save
+local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.go",
+  callback = function()
+   require('go.format').gofmt()
+  end,
+  group = format_sync_grp,
+})
