d047ba4d4770af952f317b16f17887cbf6a80a82
Mario G. Pozzo <camboris@gmail.com>
2023-05-26 14:33:22 -0600

    nvim: instala mini y keymap de sessions

---
+++ nvim/.config/nvim/lua/boris/plugins/mini.lua
@@ -0,0 +1,19 @@
+local mini = {
+  { 'echasnovski/mini.nvim',
+    version = false,
+    config = function()
+      require('mini.animate').setup({
+        scroll = {
+          enable = false,
+        }
+      })
+      require('mini.sessions').setup({})
+      vim.api.nvim_set_keymap("n", "<leader>qw", [[<cmd>lua MiniSessions.write(require("functions").sessionInput())<cr>]], {desc = "Sessions: write session"})
+      vim.api.nvim_set_keymap("n", "<leader>qr", [[<cmd>lua MiniSessions.read(require("functions").sessionInput())<cr>]], {desc = "Sessions: read a session by name"})
+      vim.api.nvim_set_keymap("n", "<leader>qs", [[<cmd>lua MiniSessions.select()<cr>]], {desc = "Sessions: Select a session"})
+      vim.api.nvim_set_keymap("n", "<leader>ql", [[<cmd>lua MiniSessions.read(MiniSessions.get_latest())<cr>]], {desc = "Sessions: Load last session"})
+    end
+  },
+}
+
+return mini
