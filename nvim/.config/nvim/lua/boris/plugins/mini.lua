3aa7aa2d9147558024130e565f3492cce8c08f14
Mario G. Pozzo <camboris@gmail.com>
2023-06-03 12:41:49 -0600

    nvim: ajustes keybindings mini sessions

--- nvim/.config/nvim/lua/boris/plugins/mini.lua
+++ nvim/.config/nvim/lua/boris/plugins/mini.lua
@@ -7,11 +7,20 @@ local mini = {
           enable = false,
         }
       })
+      require('mini.splitjoin').setup()
       require('mini.sessions').setup({})
-      vim.api.nvim_set_keymap("n", "<leader>qw", [[<cmd>lua MiniSessions.write(require("functions").sessionInput())<cr>]], {desc = "Sessions: write session"})
-      vim.api.nvim_set_keymap("n", "<leader>qr", [[<cmd>lua MiniSessions.read(require("functions").sessionInput())<cr>]], {desc = "Sessions: read a session by name"})
-      vim.api.nvim_set_keymap("n", "<leader>qs", [[<cmd>lua MiniSessions.select()<cr>]], {desc = "Sessions: Select a session"})
-      vim.api.nvim_set_keymap("n", "<leader>ql", [[<cmd>lua MiniSessions.read(MiniSessions.get_latest())<cr>]], {desc = "Sessions: Load last session"})
+      vim.api.nvim_set_keymap(
+        "n",
+        "<leader>qw",
+        [[<cmd>lua MiniSessions.write(require("functions").sessionInput())<cr>]],
+        { desc = "Sessions: write session" }
+      )
+      vim.api.nvim_set_keymap("n", "<leader>qr", [[<cmd>lua MiniSessions.read(require("functions").sessionInput())<cr>]],
+        { desc = "Sessions: read a session by name" })
+      vim.api.nvim_set_keymap("n", "<leader>qs", [[<cmd>lua MiniSessions.select()<cr>]],
+        { desc = "Sessions: Select a session" })
+      vim.api.nvim_set_keymap("n", "<leader>ql", [[<cmd>lua MiniSessions.read(MiniSessions.get_latest())<cr>]],
+        { desc = "Sessions: Load last session" })
     end
   },
 }
