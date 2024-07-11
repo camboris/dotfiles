c92bd03b9ed0c771401370ac4237bbd1661add12
Mario G. Pozzo <camboris@gmail.com>
2024-07-11 15:23:14 -0600

    quita treesitter textobjects

--- nvim/.config/nvim/lua/boris/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/boris/plugins/treesitter.lua
@@ -3,7 +3,7 @@ local M = {
   event = "BufRead",
   build = ":TSUpdate",
   dependencies = {
-    "nvim-treesitter/nvim-treesitter-textobjects",
+    -- "nvim-treesitter/nvim-treesitter-textobjects",
     -- "HiPhish/nvim-ts-rainbow2"
   },
   config = function()
@@ -35,40 +35,40 @@ local M = {
         enable = true,
       },
       autopairs = { { enable = true } },
-      textobjects = {
-        select = {
-          enable = true,
-          keymaps = {
-            ['aa'] = '@parameter.outer',
-            ['ia'] = '@parameter.inner',
-            ['af'] = '@function.outer',
-            ['if'] = '@function.inner',
-            ['ac'] = '@class.outer',
-            ['ic'] = '@class.inner',
-          },
-        },
-        swap = {
-          enable = true,
-          swap_next = { ['<leader>a'] = '@parameter.inner' },
-          swap_previous = { ['<leader>A'] = '@parameter.inner' },
-        },
-        move = {
-          enable = true,
-          set_jumps = true,
-          goto_next_start = { [']a'] = '@parameter.outer', [']f'] = '@function.outer', [']c'] = '@class.outer' },
-          goto_next_end = { [']A'] = '@parameter.outer', [']F'] = '@function.outer', [']C'] = '@class.outer' },
-          goto_previous_start = { ['[a'] = '@parameter.outer', ['[f'] = '@function.outer', ['[c'] = '@class.outer' },
-          goto_previous_end = { ['[A'] = '@parameter.outer', ['[F'] = '@function.outer', ['[C'] = '@class.outer' },
-        },
-        lsp_interop = {
-          enable = true,
-          border = 'none',
-          peek_definition_code = {
-            ["df"] = "@function.outer",
-            ["dF"] = "@class.outer",
-          },
-        },
-      },
+      -- textobjects = {
+      --   select = {
+      --     enable = true,
+      --     keymaps = {
+      --       ['aa'] = '@parameter.outer',
+      --       ['ia'] = '@parameter.inner',
+      --       ['af'] = '@function.outer',
+      --       ['if'] = '@function.inner',
+      --       ['ac'] = '@class.outer',
+      --       ['ic'] = '@class.inner',
+      --     },
+      --   },
+      --   swap = {
+      --     enable = true,
+      --     swap_next = { ['<leader>a'] = '@parameter.inner' },
+      --     swap_previous = { ['<leader>A'] = '@parameter.inner' },
+      --   },
+      --   move = {
+      --     enable = true,
+      --     set_jumps = true,
+      --     goto_next_start = { [']a'] = '@parameter.outer', [']f'] = '@function.outer', [']c'] = '@class.outer' },
+      --     goto_next_end = { [']A'] = '@parameter.outer', [']F'] = '@function.outer', [']C'] = '@class.outer' },
+      --     goto_previous_start = { ['[a'] = '@parameter.outer', ['[f'] = '@function.outer', ['[c'] = '@class.outer' },
+      --     goto_previous_end = { ['[A'] = '@parameter.outer', ['[F'] = '@function.outer', ['[C'] = '@class.outer' },
+      --   },
+      --   lsp_interop = {
+      --     enable = true,
+      --     border = 'none',
+      --     peek_definition_code = {
+      --       ["df"] = "@function.outer",
+      --       ["dF"] = "@class.outer",
+      --     },
+      --   },
+      -- },
     })
   end,
 }
