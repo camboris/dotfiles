3c569e9d1e7999c305891dc3ce56e0248f9dfd4d
Mario G. Pozzo <camboris@gmail.com>
2025-02-27 11:49:33 -0600

    ajustes Oil

--- nvim/.config/nvim/lua/boris/plugins/editor.lua
+++ nvim/.config/nvim/lua/boris/plugins/editor.lua
@@ -15,8 +15,31 @@ local neoclip = {
 
 local oil = {
   'stevearc/oil.nvim',
-  opts = {},
   cmd = { "Oil" },
+  config = function()
+    local detail = false
+    require('oil').setup({
+      default_file_explorer = true,
+      keymaps = {
+        ["q"] = { "actions.close", mode = "n" },
+        ["gd"] = {
+          desc = "Toggle file detail view",
+          callback = function()
+            detail = not detail
+            if detail then
+              require("oil").set_columns({ "icon", "permissions", "size", "mtime" })
+            else
+              require("oil").set_columns({ "icon" })
+            end
+          end,
+        },
+      },
+    })
+  end,
+  keys = {
+    { "-",    "<CMD>Oil --float --preview<CR>", desc = "Open parent directory" },
+    { "<F3>", "<CMD>Oil --float --preview<CR>", desc = "Open parent directory" },
+  },
   dependencies = { "nvim-tree/nvim-web-devicons" },
 }
 
