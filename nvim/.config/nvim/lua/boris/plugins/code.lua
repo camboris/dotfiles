21255ebf92f4eac087775146b6504470f985cd16
Mario G. Pozzo <mario.pozzo@mercadolibre.com>
2026-03-03 11:38:37 -0600

    go lsp

--- nvim/.config/nvim/lua/boris/plugins/code.lua
+++ nvim/.config/nvim/lua/boris/plugins/code.lua
@@ -1,6 +1,32 @@
+local go = {
+  "ray-x/go.nvim",
+  dependencies = { -- optional packages
+    "ray-x/guihua.lua",
+    -- "neovim/nvim-lspconfig",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  opts = function()
+    require("go").setup(opts)
+    local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      pattern = "*.go",
+      callback = function()
+        require('go.format').goimports()
+      end,
+      group = format_sync_grp,
+    })
+    return {
+      -- lsp_keymaps = false,
+      -- other options
+    }
+  end,
+  event = { "CmdlineEnter" },
+  ft = { "go", 'gomod' },
+  build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+}
 local openingh = {
   "almo7aya/openingh.nvim",
-  cmd = { "OpenInGHRepo", "OpenInGHFile", "OpenInGHFileLines"}
+  cmd = { "OpenInGHRepo", "OpenInGHFile", "OpenInGHFileLines" }
 }
 local plantuml = {
   "aklt/plantuml-syntax",
@@ -193,4 +219,5 @@ return {
   agentic,
   coverage,
   openingh,
+  -- go,
 }
